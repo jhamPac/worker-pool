68a32077c79188c310b5558b37da2b98007cd16d
jhamPac <cloud@kevia.me>
2021-07-06 16:45:02 -0700

    feat: add more workers

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	jobs := make(chan int, 100)
 	results := make(chan int, 100)
 
 	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
 
 	for i := 0; i < 100; i++ {
 		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
 	}
 }
 
