556fed0b52954aa7c10f76ea52b63ae3e9c36497
jhamPac <cloud@kevia.me>
2021-07-06 16:43:19 -0700

    feat: worker done

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 func main() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
 }
 
 func worker(jobs <-chan int, results chan<- int) {
-
+	for n := range jobs {
+		results <- fib(n)
+	}
 }
 
 func fib(n int) int {
